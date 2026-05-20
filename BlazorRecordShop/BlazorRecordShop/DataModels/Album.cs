c225427f04cdadee36cfc743f60f63ed5b673cf3
leo <62425425+lleaob@users.noreply.github.com>
2026-05-20 16:38:50 +0100

    feat: Add functionality to modify Album data in the database.

--- BlazorRecordShop/BlazorRecordShop/DataModels/Album.cs
+++ BlazorRecordShop/BlazorRecordShop/DataModels/Album.cs
@@ -1,10 +1,15 @@
-﻿namespace BlazorRecordShop.DataModels
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorRecordShop.DataModels
 {
     public class Album (int id, string artist, string title, int releaseYear)
     {
         public int Id { get; set; } = id;
+        [Required]
         public string Artist { get; set; } = artist;
+        [Required]
         public string Title { get; set; } = title;
+        [Required]
+        [Range(1948,2200)]
         public int ReleaseYear { get; set; } = releaseYear;
         public List<string> Tracklist { get; set; } = new List<string>();
         public MusicGenre Genre { get; set; } = MusicGenre.Unkown;
